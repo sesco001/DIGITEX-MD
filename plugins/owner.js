17e9e04e9204e5971ebf7d41c8450219b9749ae2
sesco001 <marcasmatheka@gmail.com>
2025-05-24 13:19:42 -0700

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -35,13 +35,13 @@ async (conn, mek, m, { from, reply }) => {
 
         // Send owner details with image
         await conn.sendMessage(from, {
-            image: { url: 'https://files.catbox.moe/yndl4u.jpg' },
+            image: { url: 'https://files.catbox.moe/g7qjxj.png' },
             caption:
 `╭─〔 *OWNER* 〕─╮
 │ 👤 ${ownerName}
 │ ☎️ ${ownerNumber}
 │ ⚙️ v2.0.0 Beta
-│ 🧠 PEACE MD
+│ 🧠 DIGITEX MD
 ╰──────────────╯
 _Only for important queries._`,
             contextInfo: {
@@ -50,7 +50,7 @@ _Only for important queries._`,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363290715861418@newsletter',
-                    newsletterName: 'PEACE MD',
+                    newsletterName: 'DIGITEX MD',
                     serverMessageId: 143
                 }
             }
@@ -58,7 +58,7 @@ _Only for important queries._`,
 
         // Send voice message
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/x9g2rd.m4a' },
+            audio: { url: 'https://files.catbox.moe/g7qjxj.png' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
